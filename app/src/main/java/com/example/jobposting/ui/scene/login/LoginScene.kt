54e2b5f88f79609f1d1fa74bdc866352051f982e
kubilay.direk <kubilay.direk@kolaysoft.com.tr>
2024-12-17 10:11:05 +0300

    string ifadeler düzenlendi. sayfaya scroll eklendi

--- app/src/main/java/com/example/jobposting/ui/scene/login/LoginScene.kt
+++ app/src/main/java/com/example/jobposting/ui/scene/login/LoginScene.kt
@@ -1,5 +1,6 @@
 package com.example.jobposting.ui.scene.login
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -11,13 +12,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,33 +35,40 @@ import com.example.jobposting.ui.theme.Dark
 import com.example.jobposting.ui.theme.DarkGrey
 import com.example.jobposting.ui.theme.customTypography
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun LoginScene(modifier: Modifier = Modifier) {
     val viewState: LoginViewState = rememberLoginViewState()
-    Surface(modifier = modifier.fillMaxSize().imePadding()) {
-        Box(
-            modifier = modifier
-                .fillMaxSize()
-                .padding(top = 96.dp, start = 16.dp, end = 16.dp, bottom = 16.dp)
-        ) {
-            Column(
-                modifier = modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(5.dp)
-            ) {
-                Image(
-                    painter = painterResource(R.drawable.cool_kids_sitting),
-                    contentDescription = null,
-                    contentScale = ContentScale.Fit,
+    Scaffold (
+        modifier = modifier.imePadding(),
+        content = {
+            Surface(modifier = modifier.fillMaxSize()) {
+                Box(
                     modifier = modifier
-                        .height(253.dp)
-                        .width(343.dp)
-                )
-                TextContent(modifier)
-                Form(modifier, viewState)
+                        .fillMaxSize()
+                        .padding(top = 96.dp, start = 16.dp, end = 16.dp, bottom = 16.dp)
+                ) {
+                    Column(
+                        modifier = modifier.fillMaxSize().verticalScroll(rememberScrollState()),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.spacedBy(5.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.cool_kids_sitting),
+                            contentDescription = null,
+                            contentScale = ContentScale.Fit,
+                            modifier = modifier
+                                .height(253.dp)
+                                .width(343.dp)
+                        )
+                        TextContent(modifier)
+                        Form(modifier, viewState)
+                    }
+                }
             }
         }
-    }
+    )
+
 }
 
 
@@ -72,8 +84,8 @@ private fun TextContent(modifier: Modifier) {
             verticalArrangement = Arrangement.spacedBy(5.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            JobText(text = "Log in", color = Dark, style = customTypography.headlineLarge)
-            JobText(text = "Login with social networks", color = DarkGrey, style = customTypography.titleSmall)
+            JobText(text = stringResource(R.string.log_in), color = Dark, style = customTypography.headlineLarge)
+            JobText(text = stringResource(R.string.login_with_social_networks), color = DarkGrey, style = customTypography.titleSmall)
             SocialButtons(modifier)
         }
     }
@@ -109,7 +121,7 @@ private fun Form(modifier: Modifier, viewState: LoginViewState) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             JobTextField(
-                label = "Email",
+                label = stringResource(R.string.email),
                 value = viewState.username.value,
                 onChanceValue = {
                     viewState.username.value = it
@@ -117,7 +129,7 @@ private fun Form(modifier: Modifier, viewState: LoginViewState) {
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
             )
             JobTextField(
-                label = "Password",
+                label = stringResource(R.string.password),
                 value = viewState.password.value,
                 onChanceValue = {
                     viewState.password.value = it
@@ -125,9 +137,9 @@ private fun Form(modifier: Modifier, viewState: LoginViewState) {
                 isPasswordField = true,
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
             )
-            JobText(text = "Forgot Password?", color = DarkGrey)
-            JobButton(buttonText = "Log in", onclick = {})
-            JobText(text = "Sign up", color = DarkGrey)
+            JobText(text = stringResource(R.string.forgot_password), color = DarkGrey)
+            JobButton(buttonText = stringResource(R.string.log_in), onclick = {})
+            JobText(text = stringResource(R.string.sign_up), color = DarkGrey)
         }
 
     }
